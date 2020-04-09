8952c160d32df0ae4ae71a2ae0bdc42877a066b4
Dan F-M <foreman.mackey@gmail.com>
2020-04-09 16:44:11 -0400

    initial commit

---
+++ src/transitory/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ["__version__"]
+
+from .transitory_version import __version__
+
+# __bibtex__ = __citation__ = CITATIONS["exoplanet"][1]
+__uri__ = "https://docs.exoplanet.codes"
+__author__ = "Daniel Foreman-Mackey"
+__email__ = "foreman.mackey@gmail.com"
+__license__ = "MIT"
+__description__ = "Quick transit fits using exoplanet"
